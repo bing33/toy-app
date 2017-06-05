582d93b9f95e9cf5d67fff239bc5e576d8d378bc
Xiaobing <xrawlinson@cottonwoodres.com>
2017-06-05 10:26:57 -0600

    added index so it links to home

--- app/routes/blog-post.js
+++ app/routes/blog-post.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    templateName: 'blog-post',
+
     model: function(params, transition) {
         return { username: params.username };
     },
